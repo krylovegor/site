8047e815df1dea48822ffa392d8d7796b9b088e3
krylovegor <krylov.egor2013@yandex.ru>
2016-10-12 10:27:38 +0300

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,13 @@
+function show_text(){
+var c0_elem = document.getElementById("c0");
+var c1_elem = document.getElementById("c1");
+var c2_elem = document.getElementById("c2");
+var c3_elem = document.getElementById("c3");
+var c4_elem = document.getElementById("c4");
+c0_elem.style.display='none';
+c1_elem.style.display='block';
+c2_elem.style.display = 'none';
+c3_elem.style.display = 'none';
+c4_elem.style.display = 'none';
+}	
+
